11476e0faeb4be61fbd1675a9a76634481c19df7
IELSMalta <99180908+IELSMalta@users.noreply.github.com>
2022-04-11 08:32:39 +0200

    Create kiosk.sh

---
+++ kiosk.sh
@@ -0,0 +1,35 @@
+sudo apt updade
+sudo apt upgrade
+sudo apt install freerdp2-x11 libavcodec-dev libasound2-dev libpulse-dev libxv-dev git xdotool xserver-xorg x11-xserver-utils xinit openbox cups hplip hplip-gui cockpit cockpit-pcp
+cd /etc/
+sudo git clone https://github.com/IELSMalta/ielskiosk.git
+adduser ###
+adduser #########
+usermod -aG sudo ###
+usermod -aG plugdev ###
+echo 'SUBSYSTEM=="usb", MODE="0660", GROUP="plugdev"' > /etc/udev/rules.d/00-usb-permissions.rules 
+udevadm control --reload-rules
+sudo mv ./kiosk.service /lib/systemd/system/kiosk.service
+sudo systemctl enable kiosk.service
+sudo systemctl start kiosk.service
+###   AUTOLOGIN ### USER   ###
+sudo systemctl edit getty@tty1.service
+#ADD#
+[Service]
+
+ExecStart=
+
+ExecStart=-/sbin/agetty --noissue --autologin ### %I $TERM
+
+Type=idle
+#END#
+nano .bash_profile
+[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx --
+/etc/default/grub
+
+
+sudo update-grub2
+GRUB_CMDLINE_LINUX_DEFAULT="quiet loglevel=3 vga=current rd.systemd.show_status=auto rd.udev.log-priority=3 vt.global_cursor_default=0"
+
+/etc/systemd/logind.conf ==> HandlePowerKey=ignore
+
